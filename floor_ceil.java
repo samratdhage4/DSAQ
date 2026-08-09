bec54a3b96dace839e467e2b4b52b6f963a123af
samratdhage4 <samratdhage4@gmail.com>
2026-08-09 18:36:01 +0530

    GFG-binary search

---
+++ floor_ceil.java
@@ -0,0 +1,63 @@
+/*
+floor is nothing but lower bound  
+0 1 2 3 4 5 6 7 8 9
+*/
+
+public class floor_ceil 
+{
+    private int floor(int[] arr,int x)
+    {
+        int i=0;
+        int j=arr.length-1;
+        int ans=-1;
+
+        while(i<=j)
+        {
+            int mid=i+(j-i)/2;
+
+            if(arr[mid]>=x)
+            {
+                ans=mid;
+                j=mid-1;
+            }else
+            {
+                i=mid+1;
+            }
+        }
+
+        return ans;
+    }
+    
+    private int ceil(int[] arr,int x)
+    {
+        int i=0;
+        int j=arr.length-1;
+        int ans=-1;
+
+        while(i<=j)
+        {
+            int mid=i+(j-i)/2;
+
+            if(arr[mid]>x)
+            {
+                ans=mid;
+                j=mid-1;
+            }else
+            {
+                i=mid+1;
+            }
+        }
+
+        return ans-1;
+    }
+
+    public static void main(String[] args) {
+        
+        floor_ceil s=new floor_ceil();
+
+        System.out.println(s.floor(new int[] {0,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,6,7,8,9}, 1));
+        System.out.println(s.ceil(new int[] {0,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,6,7,8,9},  1));
+        
+    }
+    
+}
