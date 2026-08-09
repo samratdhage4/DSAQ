71b3cd68bc62b9ef7cfd2ee6c205c0b4f0d4fd35
samratdhage4 <samratdhage4@gmail.com>
2026-08-09 16:56:47 +0530

    add lower and upper bound in binary search

--- array/subbarray_sum_0.java
+++ array/subbarray_sum_0.java
@@ -11,69 +11,32 @@ public class subbarray_sum_0 {
 
         for (int i = 0; i < arr.length; i++) {
 
-            System.out.println("\n==============================");
-            System.out.println("i = " + i);
-            System.out.println("arr[" + i + "] = " + arr[i]);
-
             sum += arr[i];
-
-            System.out.println("sum = " + sum);
 
             if (sum == 0) {
 
                 h = i + 1;
-
-                System.out.println("sum == 0");
-                System.out.println("Subarray: index 0 to " + i);
-                System.out.println("Length = " + (i + 1));
             }
-
             if (map.containsKey(sum)) {
 
                 int oldIndex = map.get(sum);
                 int length = i - oldIndex;
 
-                System.out.println("sum " + sum + " already exists!");
-                System.out.println("Old index = " + oldIndex);
-                System.out.println("Current index = " + i);
-
-                System.out.println(
-                    "Length = current index - old index"
-                );
-
-                System.out.println(
-                    length + " = " + i + " - " + oldIndex
-                );
-
                 h = Math.max(h, length);
-
-                System.out.println("h = " + h);
-
+               
             } else {
 
                 map.put(sum, i);
-
-                System.out.println(
-                    "sum " + sum + " not found, so storing:"
-                );
-
-                System.out.println(
-                    "map.put(" + sum + ", " + i + ")"
-                );
             }
 
-            System.out.println("Current map = " + map);
-            System.out.println("Current h = " + h);
+          
         }
-
-        System.out.println("\n==============================");
-        System.out.println("FINAL ANSWER = " + h);
     }
 
     public static void main(String[] args) {
 
         subbarray_sum_0 s = new subbarray_sum_0();
 
-        s.show(new int[]{1, 2, 3, -5, 0, 7, -7});
+        s.show(new int[]{15,1,-16,1,2,3,4,5});
     }
 }
