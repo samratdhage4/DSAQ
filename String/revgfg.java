7ebe3074bf20f4a602ffb1d0870d11a89c8ac3cd
samratdhage4 <samratdhage4@gmail.com>
2026-08-13 23:44:03 +0530

    GFG- revers String

---
+++ String/revgfg.java
@@ -0,0 +1,16 @@
+public class revgfg {
+    
+    public static String reverseString(String s) {
+       
+       
+       StringBuilder res= new StringBuilder();
+       
+       for(int i=s.length()-1;i>=0;i--)
+       {
+           
+           res.append(s.charAt(i));
+       }
+        
+        return res.toString();
+    }
+}
