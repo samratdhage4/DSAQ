cb50e23fb31de103298f26d691cfaa54da01bfd5
samratdhage4 <samratdhage4@gmail.com>
2026-08-13 23:43:04 +0530

    LeetCode-796 compare rotates string

---
+++ String/rotate.java
@@ -0,0 +1,47 @@
+
+/*
+796. Rotate String
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+ 
+
+Example 1:
+
+Input: s = "abcde", goal = "cdeab"
+Output: true
+Example 2:
+
+Input: s = "abcde", goal = "abced"
+Output: false
+ 
+*/
+public class rotate {
+    
+   
+    public boolean rotateString(String s, String goal) 
+    {
+        if(s.length()!=goal.length()) return false;
+
+        String res= goal+goal;
+
+        return (res.contains(s)); 
+        
+    }
+
+    public static void main(String[] args) {
+        rotate  r= new rotate();
+
+        System.out.println(r.rotateString("abcde", "cdeab"));
+    }
+}
+    
+
+
