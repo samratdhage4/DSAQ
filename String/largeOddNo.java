8f134930da1d2e4f5edd2f8e885ec7c2744eae88
samratdhage4 <samratdhage4@gmail.com>
2026-08-15 00:28:48 +0530

    LeetCode-1902 largest odd no in Srting

--- String/largeOddNo.java
+++ String/largeOddNo.java
@@ -18,12 +18,10 @@ Input: num = "52"
 Output: "5"
 Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the only odd number.
 Example 2:
-
 Input: num = "4206"
 Output: ""
 Explanation: There are no odd numbers in "4206".
 Example 3:
-
 Input: num = "35427"
 Output: "35427"
 Explanation: "35427" is already an odd number.
@@ -51,8 +49,6 @@ public class largeOddNo
     }
 
     public static void main(String[] args) {
-        
-
         largeOddNo l= new largeOddNo();
         System.out.println(l.s("1234567"));
     }
