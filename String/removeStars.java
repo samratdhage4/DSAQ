1b812c8817f42e7dca9bb5d2c6cdd464c6521e23
samratdhage4 <samratdhage4@gmail.com>
2026-09-01 23:29:30 +0530

    2390. Removing Stars From a String

---
+++ String/removeStars.java
@@ -0,0 +1,104 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+2390. Removing Stars From a String
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You are given a string s, which contains stars *.
+
+In one operation, you can:
+
+Choose a star in s.
+Remove the closest non-star character to its left, as well as remove the star itself.
+Return the string after all stars have been removed.
+
+Note:
+
+The input will be generated such that the operation is always possible.
+It can be shown that the resulting string will always be unique.
+ 
+
+Example 1:
+
+Input: s = "leet**cod*e"
+Output: "lecoe"
+Explanation: Performing the removals from left to right:
+- The closest character to the 1st star is 't' in "leet**cod*e". s becomes "lee*cod*e".
+- The closest character to the 2nd star is 'e' in "lee*cod*e". s becomes "lecod*e".
+- The closest character to the 3rd star is 'd' in "lecod*e". s becomes "lecoe".
+There are no more stars, so we return "lecoe".
+Example 2:
+
+Input: s = "erase*****"
+Output: ""
+Explanation: The entire string is removed, so we return an empty string.
+
+*/
+
+
+public class removeStars 
+{
+     static String removeStars(String s) 
+    {
+    //    StringBuilder sb = new StringBuilder();
+
+    //     for(int i=0;i<s.length();i++)
+    //     {
+    //         char c= s.charAt(i);
+
+    //         if(c=='*')
+    //         {
+    //             sb.deleteCharAt(sb.length()-1);
+    //         }else
+    //         {
+    //             sb.append(c);
+    //         }
+    //     }
+        
+    //     return sb.toString();
+
+        char[] stk= new char[s.length()];
+        int top=0;
+
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+
+            if(c=='*')
+            {
+                top--;
+            }else
+            {
+                stk[top++]=c;
+            }
+        }
+
+        return new String(stk,0,top);
+
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        
+
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("ENTER THE STRING");
+       String s = br.readLine();
+       String res = removeStars(s);
+        System.out.println(s + " After remove * :- " + res);
+
+        br.close();
+
+    
+        
+    }
+}
+    
+
