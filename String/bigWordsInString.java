29b7ce51f325ee29fe3f2442f5ffabd32e297b7a
samratdhage4 <samratdhage4@gmail.com>
2026-09-15 13:49:27 +0530

    maximum length of word in given String

---
+++ String/bigWordsInString.java
@@ -0,0 +1,26 @@
+import  java.util.Scanner;
+public class bigWordsInString {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc= new Scanner(System.in);
+
+        String str=sc.nextLine();
+
+        String[] word=str.split("[    .]+");
+
+        int max=0;
+        for(String s:word)
+        {
+            max=Math.max(max, s.length());
+        }
+        for(String s:word)
+        {
+            if(s.length()==max)
+            {
+                System.out.println(s);
+            }
+        }
+        sc.close();
+    }
+}
